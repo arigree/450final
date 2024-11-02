6da1f443aef77b463332877cf037b184f9903cb2
arigree <arigree@iu.edu>
2024-11-02 16:10:54 -0400

    initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
-
 import "./globals.css";
+import Nav from "@/components/Nav";
 
 export const metadata = {
   title: "Create Next App",
@@ -9,7 +9,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body></body>
+      <body>
+        
+          <Nav />
+          {children}
+        
+      </body>
     </html>
   );
 }
